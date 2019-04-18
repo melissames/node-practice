b832ab2335f10579116c9bdb0725727568afad5c
github username <melissa.sames@gmail.com>
2019-04-18 19:21:29 -0400

    added api calls to express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+const dotenv = require('dotenv')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 
 const app = express()
+dotenv.config()
 
+//setup handlebars views location
 app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, '../templates/views'))
+hbs.registerPartials(path.join(__dirname, '../templates/partials'))
+
+//setup for static directories
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('', (req, res) => {
@@ -22,14 +33,35 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
+        title: 'Help',
+        name: 'Melissa',
         message: 'What can I help you with?'
     })
 })
 
 app.get('/weather', (req, res) => {
-    res.send({
-        forecast: 'sunny',
-        location: 'New York'
+    if(!req.query.address){
+        return res.send({
+            error: 'You must provide a address'
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if(error){
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
