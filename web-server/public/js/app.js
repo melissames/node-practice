7cb5c45bc7e9fe345f2f1fd91074ebae6effceb2
github username <melissa.sames@gmail.com>
2019-04-18 21:06:10 -0400

    created ui and fetch call

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,29 @@
 console.log('client side js file is loaded')
+
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.forecast
+                messageTwo.textContent = data.location
+            }
+        })
+    })
+})
